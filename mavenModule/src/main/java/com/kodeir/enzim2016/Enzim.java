8fa9380aa703af9f63bb29d87c8f324f19b4bb59
kodeir <kodeir@gmail.com>
2016-06-10 16:32:42 +0300

    bd

---
+++ mavenModule/src/main/java/com/kodeir/enzim2016/Enzim.java
@@ -0,0 +1,11 @@
+package com.kodeir.enzim2016;
+
+/**
+ * Created by rowan on 5/31/16.
+ */
+public class Enzim {
+
+    public static void main(String[] args) {
+        System.out.println("Enzim started");
+    }
+}
---
+++ mavenModule/src/test/java/com/kodeir/enzim2016/EnzimTest.java
@@ -0,0 +1,15 @@
+package com.kodeir.enzim2016;
+
+import org.junit.Test;
+
+/**
+ * Created by rowan on 5/31/16.
+ */
+public class EnzimTest {
+
+    @Test
+    public void testEnzimMain(){
+        Enzim.main(new String[] {});
+    }
+
+}
