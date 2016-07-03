35960ccc087f8976915de9d8b37dcd8a79c9eaf5
kodeir <kodeir@gmail.com>
2016-07-03 14:23:04 +0300

    SwingApp

--- mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/SwingApp.java
+++ mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/SwingApp.java
@@ -9,6 +9,11 @@ import java.awt.*;
 public class SwingApp {
 
     private JFrame frame;
+    private GridBagConstraints constraints;
+    private final int HORIZONTAL = GridBagConstraints.HORIZONTAL;
+
+    private JButton newPatientBtn;
+    private JButton openDatabaseBtn;
 
     public static void startSwing() {
         EventQueue.invokeLater(() -> {
@@ -19,6 +24,35 @@ public class SwingApp {
                 e.printStackTrace();
             }
         });
+    }
+
+    public SwingApp(){
+        initialize();
+    }
+
+    private void initialize(){
+        frame = new JFrame("Enzim 2016");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addComponentsToPane(frame.getContentPane());
+    }
+
+    private void addComponentsToPane(Container pane){
+        pane.setLayout(new GridBagLayout());
+
+        newPatientBtn = new JButton("Button 1");
+        pane.add(newPatientBtn, setConstraints(0.5, HORIZONTAL, 0, 0));
+
+        openDatabaseBtn = new JButton("Button 1");
+        pane.add(openDatabaseBtn, setConstraints(0.5, HORIZONTAL, 1, 0));
+
+    }
+
+    private GridBagConstraints setConstraints(double weightx, int fill, int gridx, int gridy){
+        constraints.weightx = weightx;
+        constraints.fill = fill;
+        constraints.gridx = gridx;
+        constraints.gridy = gridy;
+        return constraints;
     }
 
 
