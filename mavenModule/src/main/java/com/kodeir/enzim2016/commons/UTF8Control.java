b4d6f3b80168115d8b0ef5db322da298ea977aeb
kodeir <kodeir@gmail.com>
2016-07-24 12:05:49 +0300

    UTF8Control javadoc

--- mavenModule/src/main/java/com/kodeir/enzim2016/commons/UTF8Control.java
+++ mavenModule/src/main/java/com/kodeir/enzim2016/commons/UTF8Control.java
@@ -10,15 +10,19 @@ import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 /**
- * http://javatalks.ru/topics/28413
+ * Extends ResourceBundle.Control to override newBundle method with the use of UTF-8
+ * Code taken from http://javatalks.ru/topics/28413
  */
 public class UTF8Control extends ResourceBundle.Control {
 
+    /**
+     * "UTF-8" on line (#45) to support UTF-8 properties.
+     */
+    @Override
     public ResourceBundle newBundle
             (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
             throws IllegalAccessException, InstantiationException, IOException
     {
-        // The below is a copy of the default implementation.
         String bundleName = toBundleName(baseName, locale);
         String resourceName = toResourceName(bundleName, "properties");
         ResourceBundle bundle = null;
@@ -37,7 +41,6 @@ public class UTF8Control extends ResourceBundle.Control {
         }
         if (stream != null) {
             try {
-                // Only this line is changed to make it to read properties files as UTF-8.
                 bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
             } finally {
                 stream.close();
