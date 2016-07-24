20c1491ad9052003db6e2c726101b4d3fa1742c4
kodeir <kodeir@gmail.com>
2016-07-24 17:44:26 +0300

    UTF8Control updated with fresh code

--- mavenModule/src/main/java/com/kodeir/enzim2016/commons/UTF8Control.java
+++ mavenModule/src/main/java/com/kodeir/enzim2016/commons/UTF8Control.java
@@ -5,47 +5,99 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 /**
  * Extends ResourceBundle.Control to override newBundle method with the use of UTF-8
- * Code taken from http://javatalks.ru/topics/28413
+ * Idea taken from http://javatalks.ru/topics/28413
  */
 public class UTF8Control extends ResourceBundle.Control {
 
     /**
-     * "UTF-8" on line (#45) to support UTF-8 properties.
+     * InputStreamReader with "UTF-8" on line (#81) to support UTF-8 properties with cyrillic characters.
      */
     @Override
-    public ResourceBundle newBundle
-            (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
-            throws IllegalAccessException, InstantiationException, IOException
-    {
+    public ResourceBundle newBundle(String baseName, Locale locale, String format,
+                                    ClassLoader loader, boolean reload)
+            throws IllegalAccessException, InstantiationException, IOException {
         String bundleName = toBundleName(baseName, locale);
-        String resourceName = toResourceName(bundleName, "properties");
         ResourceBundle bundle = null;
-        InputStream stream = null;
-        if (reload) {
-            URL url = loader.getResource(resourceName);
-            if (url != null) {
-                URLConnection connection = url.openConnection();
-                if (connection != null) {
-                    connection.setUseCaches(false);
-                    stream = connection.getInputStream();
+        if (format.equals("java.class")) {
+            try {
+                @SuppressWarnings("unchecked")
+                Class<? extends ResourceBundle> bundleClass
+                        = (Class<? extends ResourceBundle>)loader.loadClass(bundleName);
+
+                // If the class isn't a ResourceBundle subclass, throw a
+                // ClassCastException.
+                if (ResourceBundle.class.isAssignableFrom(bundleClass)) {
+                    bundle = bundleClass.newInstance();
+                } else {
+                    throw new ClassCastException(bundleClass.getName()
+                            + " cannot be cast to ResourceBundle");
+                }
+            } catch (ClassNotFoundException e) {
+            }
+        } else if (format.equals("java.properties")) {
+            final String resourceName = toResourceName0(bundleName, "properties");
+            if (resourceName == null) {
+                return bundle;
+            }
+            final ClassLoader classLoader = loader;
+            final boolean reloadFlag = reload;
+            InputStream stream = null;
+            try {
+                stream = AccessController.doPrivileged(
+                        new PrivilegedExceptionAction<InputStream>() {
+                            public InputStream run() throws IOException {
+                                InputStream is = null;
+                                if (reloadFlag) {
+                                    URL url = classLoader.getResource(resourceName);
+                                    if (url != null) {
+                                        URLConnection connection = url.openConnection();
+                                        if (connection != null) {
+                                            // Disable caches to get fresh data for
+                                            // reloading.
+                                            connection.setUseCaches(false);
+                                            is = connection.getInputStream();
+                                        }
+                                    }
+                                } else {
+                                    is = classLoader.getResourceAsStream(resourceName);
+                                }
+                                return is;
+                            }
+                        });
+            } catch (PrivilegedActionException e) {
+                throw (IOException) e.getException();
+            }
+            if (stream != null) {
+                try {
+                    bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+                } finally {
+                    stream.close();
                 }
             }
         } else {
-            stream = loader.getResourceAsStream(resourceName);
-        }
-        if (stream != null) {
-            try {
-                bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
-            } finally {
-                stream.close();
-            }
+            throw new IllegalArgumentException("unknown format: " + format);
         }
         return bundle;
+    }
+
+    /**
+     * copied from ResourceBundle.java
+     */
+    private String toResourceName0(String bundleName, String suffix) {
+        // application protocol check
+        if (bundleName.contains("://")) {
+            return null;
+        } else {
+            return toResourceName(bundleName, suffix);
+        }
     }
 }
