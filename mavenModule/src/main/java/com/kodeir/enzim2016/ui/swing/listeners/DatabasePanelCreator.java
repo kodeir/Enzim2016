13f82ccffbddf714e9d61119634c4574c1bfa311
kodeir <kodeir@gmail.com>
2016-09-25 17:44:44 +0300

    coefficients list

--- mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/listeners/DatabasePanelCreator.java
+++ mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/listeners/DatabasePanelCreator.java
@@ -46,25 +46,30 @@ public class DatabasePanelCreator {
         if (connectToDatabase()){
             database.setStatement();
             List<Patient> patients = new ArrayList<>();
-            ResultSet rs = database.runSelectQuery("SELECT * " +
-                    "FROM PATIENTS P " +
-                    "JOIN COEFFICIENTS C " +
-                    "ON P.patient_id = C.patient_id");
+            ResultSet rs = database.runSelectQuery("SELECT * FROM PATIENTS P JOIN COEFFICIENTS C ON P.patient_id = C.patient_id ORDER BY P.patient_id ASC");
             if (rs == null) {
                 JOptionPane.showMessageDialog(null,"0");
             } else {
                 try {
+                    long check = -1;
+                    int position = -1;
+
                     while (rs.next()) {
-                        Coefficients coefficients = new Coefficients(rs.getLong("COEFFICIENT_ID"), rs.getLong("PATIENT_ID"),
-                                rs.getFloat("AST"), rs.getFloat("ALT"), rs.getFloat("KFK"), rs.getFloat("LDG"),
-                                rs.getFloat("SHF"), rs.getFloat("GGTP"), rs.getFloat("HE"), rs.getFloat("GLDG"),
-                                rs.getDate("CHECKUP_DATE").toLocalDate());
-                        List<Coefficients> coefficientses = new ArrayList<>();
-                        coefficientses.add(coefficients);
-                        patients.add(new Patient(rs.getLong("PATIENT_ID"),
-                                rs.getString("NAME"), rs.getString("SURNAME"), rs.getString("PATRONYMIC"),
-                                rs.getDate("BIRTHDATE").toLocalDate(),
-                                coefficientses));
+
+                        long patientId = rs.getLong("PATIENT_ID");
+                        if (patientId == check){
+                            patients.get(position).getCoefficients().add(setCoefficients(rs));
+                        } else {
+                            check = patientId;
+                            Coefficients coefficients = setCoefficients(rs);
+                            List<Coefficients> coefficientses = new ArrayList<>();
+                            coefficientses.add(coefficients);
+                            patients.add(new Patient(patientId,
+                                    rs.getString("NAME"), rs.getString("SURNAME"), rs.getString("PATRONYMIC"),
+                                    rs.getDate("BIRTHDATE").toLocalDate(),
+                                    coefficientses));
+                            position = patients.size() - 1;
+                        }
                     }
                 } catch (SQLException e) {
                     JOptionPane.showMessageDialog(null, e.getMessage());
@@ -73,6 +78,13 @@ public class DatabasePanelCreator {
             return patients;
         }
         return new ArrayList<>();
+    }
+
+    private Coefficients setCoefficients(ResultSet rs) throws SQLException {
+        return new Coefficients(rs.getLong("COEFFICIENT_ID"), rs.getLong("PATIENT_ID"),
+                                        rs.getFloat("AST"), rs.getFloat("ALT"), rs.getFloat("KFK"), rs.getFloat("LDG"),
+                                        rs.getFloat("SHF"), rs.getFloat("GGTP"), rs.getFloat("HE"), rs.getFloat("GLDG"),
+                                        rs.getDate("CHECKUP_DATE").toLocalDate());
     }
 
     private boolean connectToDatabase(){
