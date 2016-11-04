86a7750de1009d7831fbd92180d4a33ebe1d0d46
kodeir <kodeir@gmail.com>
2016-11-04 14:47:22 +0300

    AboutPanelListener

---
+++ mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/listeners/AboutPanelListener.java
@@ -0,0 +1,26 @@
+package com.kodeir.enzim2016.ui.swing.listeners;
+
+import com.kodeir.enzim2016.commons.EnzimLogger;
+import com.kodeir.enzim2016.ui.swing.panels.AboutPanel;
+import com.kodeir.enzim2016.ui.swing.panels.EnzimPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Sergei Riabinin on 04.11.2016.
+ */
+public class AboutPanelListener implements ActionListener{
+    private static final EnzimLogger logger = new EnzimLogger(EnzimPanel.class.getName());
+
+    private AboutPanel aboutPanel;
+
+    public AboutPanelListener(AboutPanel aboutPanel){
+        this.aboutPanel = aboutPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //if (e.getSource().equals(aboutPanel)
+    }
+}
