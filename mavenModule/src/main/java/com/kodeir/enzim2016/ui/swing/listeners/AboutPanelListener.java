f833bf4e0eeebfedafed7221eec42e3b368988ec
kodeir <kodeir@gmail.com>
2016-11-04 15:05:37 +0300

    TutorialPanel

--- mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/listeners/AboutPanelListener.java
+++ mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/listeners/AboutPanelListener.java
@@ -1,17 +1,22 @@
 package com.kodeir.enzim2016.ui.swing.listeners;
 
 import com.kodeir.enzim2016.commons.EnzimLogger;
+import com.kodeir.enzim2016.commons.UTF8Control;
+import com.kodeir.enzim2016.ui.swing.commons.EnzimFrame;
 import com.kodeir.enzim2016.ui.swing.panels.AboutPanel;
 import com.kodeir.enzim2016.ui.swing.panels.EnzimPanel;
+import com.kodeir.enzim2016.ui.swing.panels.TutorialPanel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
 
 /**
  * Created by Sergei Riabinin on 04.11.2016.
  */
 public class AboutPanelListener implements ActionListener{
     private static final EnzimLogger logger = new EnzimLogger(EnzimPanel.class.getName());
+    private ResourceBundle rb = ResourceBundle.getBundle("rb", new UTF8Control());
 
     private AboutPanel aboutPanel;
 
@@ -27,6 +32,6 @@ public class AboutPanelListener implements ActionListener{
     }
 
     private void createTutorialPanel() {
-
+        new EnzimFrame(rb.getString("interface.tutorial"), new TutorialPanel());
     }
 }
--- mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/panels/TutorialPanel.java
+++ mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/panels/TutorialPanel.java
@@ -4,6 +4,7 @@ import com.kodeir.enzim2016.commons.EnzimLogger;
 import com.kodeir.enzim2016.commons.UTF8Control;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ResourceBundle;
 
 /**
@@ -18,17 +19,48 @@ public class TutorialPanel extends JPanel {
     }
 
     public TutorialPanel(int panelId){
+        setupPanel();
         switch (panelId){
             // 0 - New Patient Panel
-            case 0: {}
+            case 0: addPatientPanelGuide();
             // 1 - Database Panel
-            case 1: {}
+            case 1: addDatabasePanelGuide();
             // 2 - Diagnose Tree Panel
-            case 2: {}
+            case 2: addTreeGuide();
             // 3 - New Coefficients Panel
-            case 3: {}
+            case 3: addNewCoefficientsGuide();
             // 4 - About Panel
-            case 4: {}
+            case 4: {
+                addMainMenuGuide();
+                addPatientPanelGuide();
+                addDatabasePanelGuide();
+                addTreeGuide();
+                addNewCoefficientsGuide();
+            }
         }
+    }
+
+    private void setupPanel() {
+        this.setLayout(new GridBagLayout());
+    }
+
+    private void addMainMenuGuide() {
+
+    }
+
+    private void addPatientPanelGuide() {
+
+    }
+
+    private void addDatabasePanelGuide() {
+
+    }
+
+    private void addTreeGuide() {
+
+    }
+
+    private void addNewCoefficientsGuide() {
+
     }
 }
