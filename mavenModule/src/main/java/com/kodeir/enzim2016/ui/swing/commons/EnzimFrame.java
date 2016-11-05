63e194b7ef77828a81adee0fe5310a58b3ff4e1c
kodeir <kodeir@gmail.com>
2016-11-05 14:08:00 +0300

    javadoc

--- mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/commons/EnzimFrame.java
+++ mavenModule/src/main/java/com/kodeir/enzim2016/ui/swing/commons/EnzimFrame.java
@@ -9,6 +9,8 @@ import java.util.logging.Level;
 
 /**
  * Created by Sergei Riabinin on 24.07.2016.
+ *
+ * JFrame which is extended with some default settings for the program's needs.
  */
 public class EnzimFrame extends JFrame {
 
@@ -20,12 +22,29 @@ public class EnzimFrame extends JFrame {
         invoke(panel, 0);
     }
 
+    /**
+     *
+     * @param frameName frame's name
+     * @param panel panel to be added to ContentPane
+     * @param helpMap int constant (help panel mapping) to be passed to KeyboardListener
+     * @see com.kodeir.enzim2016.data.HelpMapping
+     */
     public EnzimFrame(String frameName, JPanel panel, int helpMap){
         super(frameName);
         logger.log(Level.INFO, "Mapped EnzimFrame to be created;");
         invoke(panel, helpMap);
     }
 
+    /**
+     * Method places frame to the center of the screen,
+     * Resizable = false,
+     * Focused for KeyListener
+     *
+     * @param panel panel to be added to ContentPane
+     * @param helpMap int constant (help panel mapping) to be passed to KeyboardListener
+     * @see com.kodeir.enzim2016.data.HelpMapping
+     * @see com.kodeir.enzim2016.ui.swing.listeners.KeyboardListener
+     */
     private void invoke(JPanel panel, int helpMap){
         SwingUtilities.invokeLater(() -> {
             getContentPane().add(panel);
